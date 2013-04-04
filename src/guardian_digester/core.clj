edee9cb621a141b1d95f5e88f1f943a64ecb1006
Mike Roberts <mike.b.roberts@gmail.com>
2013-04-04 19:26:11 -0400

    Minor updates

--- src/guardian_digester/core.clj
+++ src/guardian_digester/core.clj
@@ -1,5 +1,5 @@
 (ns guardian-digester.core
-	(:require [clj-http.client :as client] [clojure.data.json :as json])
+	(:require [clj-http.client :as client])
 	(:use hiccup.core))
 
 ; ** For retrieving from Guardian API
--- test/guardian_digester/core_test.clj
+++ test/guardian_digester/core_test.clj
@@ -4,18 +4,18 @@
         guardian-digester.core
         hiccup.core))
 
-(defn load-from-file []
-	(clojure.walk/keywordize-keys (json/read-str (slurp "test/guardian_digester/example.json"))))
+; (defn load-from-file []
+; 	(clojure.walk/keywordize-keys (json/read-str (slurp "test/guardian_digester/example.json"))))
 
-(defn write-to-file [x]
-	(spit "test.html" x))
+; (defn write-to-file [x]
+; 	(spit "test.html" x))
 
-(defn run-file-to-file []
-	(write-to-file (make-html (load-from-file))))
+; (defn run-file-to-file []
+; 	(write-to-file (make-html (load-from-file))))
 
-(defn r []
-	(make-html (load-from-file)))
+; (defn r []
+; 	(make-html (load-from-file)))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "ToDo - write some tests!" true))
+    ; (is (= 0 1))))
